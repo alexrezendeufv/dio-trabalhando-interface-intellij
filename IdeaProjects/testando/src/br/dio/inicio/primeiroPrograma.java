90e53edcd0e1cfd63bc4c77b73bc77143f9d583f
alexzilla <alexrezendeufv@gmail.com>
2021-12-16 06:44:21 -0800

    commit manual

---
+++ IdeaProjects/testando/src/br/dio/inicio/primeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.dio.inicio;
+
+import br.dio.inicio.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("alexandre", "azull", 23);
+        System.out.println(gato.getNome());
+    }
+}
